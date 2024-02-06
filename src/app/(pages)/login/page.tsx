7af758cd2ca38f06948f13158303a0e659aca53e
pranavpatil0335@gmail.com <pranavpatil0335@gmail.com>
2024-02-07 00:01:46 +0530

    final change

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -21,7 +21,7 @@ export default async function Login() {
       <div className={classes.heroImg}>
         <Link href="/">
           <Image
-            src="/logo-black.svg"
+            src="/black.png"
             alt="logo"
             width={250}
             height={23}
